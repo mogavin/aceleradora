ecf247adb444dca3baad8e5a5a62f9c3f6872856
mogavin <motagavino@gmail.com>
2022-12-20 09:36:06 -0300

    feat: Add calculadora prototype

---
+++ calculadora/src/App.js
@@ -0,0 +1,15 @@
+import Calculadora from './components/Calculadora'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Calculadora />
+      </header>
+    </div>
+  );
+}
+
+export default App;
